847ffe867fa1e2f50826a474da85d96020cde1b4
Ani  Chijioke <anichijioke81@gmail.com>
2025-03-09 13:45:00 +0000

    Update full_load.py

--- full_load.py
+++ full_load.py
@@ -4,12 +4,12 @@ import pyspark.sql.functions as F
 
 spark = SparkSession.builder.master("local").appName("team_pyspark").enableHiveSupport().getOrCreate()
 
-df = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.170.23.150:5432/testdb").option("driver", "org.postgresql.Driver").option("dbtable", "tfl1").option("user", "consultants").option("password", "WelcomeItc@2022").load()
+df = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.170.23.150:5432/testdb").option("driver", "org.postgresql.Driver").option("dbtable", "new_tfl1").option("user", "consultants").option("password", "WelcomeItc@2022").load()
 df.printSchema()
 
 # Step 2: Transform - Clean and Format the Data
     # Convert 'Timestamp' to proper timestamp format
-df_transformed = df.withColumn("Timestamp", F.to_timestamp(col("time"), "dd/MM/yyyy HH:mm"))
+df_transformed = df.withColumn("Timestamp", F.to_timestamp(col("timestamp"), "dd/MM/yyyy HH:mm"))
 
     # Replace "N/A" with null
 df_transformed = df.replace("N/A", None)
