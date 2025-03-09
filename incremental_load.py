8f1b880ffd0bb37e5d0013c465c9af653bc4606e
Ani  Chijioke <anichijioke81@gmail.com>
2025-03-09 19:52:00 +0000

    Update incremental_load.py

--- incremental_load.py
+++ incremental_load.py
@@ -3,12 +3,14 @@ from pyspark.sql.functions import *
 
 spark = SparkSession.builder.master("local").appName("MiniProj").enableHiveSupport().getOrCreate()
 
-max_vehicle_id = spark.sql("SELECT max(vehicle_id) FROM bigdata_nov_2024.stolen_vehicles")
-m_vehicle_id = max_vehicle_id.collect()[0][0]
+max_uid = spark.sql("SELECT max(uid) FROM big_data_jan2025.tfl_underground_pysparks")
+max_uid = max_uid.collect()[0][0]
 
-query = 'SELECT * FROM stolen_cars WHERE vehicle_id > ' + str(m_vehicle_id)
+query = 'SELECT * FROM new_tfl2 WHERE uid > ' + str(max_uid)
 
-more_data = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.132.73.146:5432/testdb").option("driver", "org.postgresql.Driver").option("user", "consultants").option("password", "mailto:welcomeitc@2022").option("query", query).load()
+more_data = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.170.23.150:5432/testdb").option("driver", "org.postgresql.Driver").option("user", "consultants").option("password", "WelcomeItc@2022").option("query", query).load()
+
+
 
 more_data.write.mode("append").saveAsTable("bigdata_nov_2024.stolen_vehicles")
 print("Successfully Load to Hive")
