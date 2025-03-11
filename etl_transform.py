25c9c8981cad6d1acbb9283bc2b0dca81b022a4d
Ani  Chijioke <anichijioke81@gmail.com>
2025-03-11 19:01:19 +0000

    Update etl_transform.py

--- etl_transform.py
+++ etl_transform.py
@@ -28,24 +28,24 @@ df_with_id = df_with_id.withColumn("route", regexp_replace(col("route"), r'^[\'"
 # Remove NULL values from the route column
 df_with_id = df_with_id.filter(col("route").isNotNull())
 
-# Retrieve the maximum existing order_id from the target table
+# Retrieve the maximum existing record_id from the target table
 try:
-    max_order_id = spark.sql("SELECT MAX(order_id) FROM {}.{}".format(HIVE_DB, TARGET_TABLE)).collect()[0][0]
+    max_order_id = spark.sql("SELECT MAX(record_id) FROM {}.{}".format(HIVE_DB, TARGET_TABLE)).collect()[0][0]
     if max_order_id is None:
         max_order_id = 0  # If table is empty, start from 1
 except:
     max_order_id = 0  # If table doesn't exist, start from 1
 
-# Generate an auto-incremented order_id for new records
+# Generate an auto-incremented record_id for new records
 window_spec = Window.orderBy(monotonically_increasing_id())
-df_with_id = df_with_id.withColumn("order_id", row_number().over(window_spec) + max_order_id)
+df_with_id = df_with_id.withColumn("record_id", row_number().over(window_spec) + max_order_id)
 
 # Debugging: Print the actual column names before selecting
 print("DataFrame Columns: ", df_with_id.columns)
 df_with_id.printSchema()
 
 # Ensure column order matches Hive table
-expected_columns = ["order_id", "timedetails", "line", "status", "reason", "delay_time", "route", "ingestion_timestamp"]  # Update this based on Hive
+expected_columns = ["record_id", "timedetails", "line", "status", "reason", "delay_time", "route", "ingestion_timestamp"]  # Update this based on Hive
 df_with_id = df_with_id.select(*expected_columns)
 
 # Append data into the existing Hive table
