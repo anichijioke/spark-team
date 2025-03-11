2d9a8709db496561bf2eaca122a452acbc80e5a2
Ani  Chijioke <anichijioke81@gmail.com>
2025-03-11 16:06:44 +0000

    Update etl_transform.py

--- etl_transform.py
+++ etl_transform.py
@@ -12,7 +12,7 @@ HIVE_DB = "default"
 SOURCE_TABLE = "tfl_undergroundrecord"
 TARGET_TABLE = "tfl_underground_result"
 
-# Load data from the source table (Use .format() instead of f-strings)
+# Load data from the source table
 df_source = spark.sql("SELECT * FROM {}.{}".format(HIVE_DB, SOURCE_TABLE))
 
 # Add a new 'record_id' column (unique identifier)
@@ -24,11 +24,24 @@ df_with_id = df_with_id.withColumn("ingestion_timestamp", current_timestamp())
 # Remove all leading and trailing quotes from the "route" column
 df_with_id = df_with_id.withColumn("route", regexp_replace(col("route"), r'^[\'"]+|[\'"]+$', ''))
 
-# Remove NULL values from route column
+# Remove NULL values from the route column
 df_with_id = df_with_id.filter(col("route").isNotNull())
 
-# Write data into the target table
-df_with_id.write.mode("overwrite").format("hive").saveAsTable("{}.{}".format(HIVE_DB, TARGET_TABLE))
+# Check if the target table exists in Hive
+table_check = spark.sql("SHOW TABLES IN {}".format(HIVE_DB)).filter(col("tableName") == TARGET_TABLE).count()
+
+# If the table does not exist, create it
+if table_check == 0:
+    spark.sql("""
+        CREATE TABLE {}.{} (
+            route STRING,
+            record_id BIGINT,
+            ingestion_timestamp TIMESTAMP
+        ) STORED AS PARQUET
+    """.format(HIVE_DB, TARGET_TABLE))
+
+# Append data instead of overwriting
+df_with_id.write.mode("append").format("hive").saveAsTable("{}.{}".format(HIVE_DB, TARGET_TABLE))
 
 # Stop Spark session
 spark.stop()
