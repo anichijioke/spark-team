7f463a8c7acf9147fe790c0f0ba09e29b27e0e1a
Ani  Chijioke <anichijioke81@gmail.com>
2025-03-11 15:58:18 +0000

    Update etl_transform.py

--- etl_transform.py
+++ etl_transform.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, current_timestamp, monotonically_increasing_id
+from pyspark.sql.functions import col, current_timestamp, monotonically_increasing_id, regexp_replace
 
 # Create Spark session with Hive support
 spark = SparkSession.builder \
@@ -9,32 +9,26 @@ spark = SparkSession.builder \
 
 # Define database and tables
 HIVE_DB = "default"
-SOURCE_TABLE = " tfl_undergroundrecord"
+SOURCE_TABLE = "tfl_undergroundrecord"
 TARGET_TABLE = "tfl_underground_result"
 
-# Use the Hive database
-# spark.sql(f"USE {HIVE_DB}")
-
 # Load data from the source table
-df_source = spark.sql("SELECT * FROM default.tfl_undergroundrecord")
+df_source = spark.sql(f"SELECT * FROM {HIVE_DB}.{SOURCE_TABLE}")
 
 # Add a new 'record_id' column (unique identifier)
 df_with_id = df_source.withColumn("record_id", monotonically_increasing_id())
 
-# 3. Adding an "ingestion_timestamp" column
+# Add an "ingestion_timestamp" column
 df_with_id = df_with_id.withColumn("ingestion_timestamp", current_timestamp())
 
-# Remove quotes from the "route" column
-# Filtering records based on a condition (Example: removing NULL values)
+# Remove all leading and trailing quotes from the "route" column
+df_with_id = df_with_id.withColumn("route", regexp_replace(col("route"), r'^[\'"]+|[\'"]+$', ''))
+
+# Remove NULL values from route column
 df_with_id = df_with_id.filter(col("route").isNotNull())
 
 # Write data into the target table
-#df_with_id.write.mode("append").saveAsTable(TARGET_TABLE)
-df_with_id.write.mode("append").format("hive").saveAsTable(TARGET_TABLE)
-
-
-# print(f"Data successfully inserted from {SOURCE_TABLE} to {TARGET_TABLE} with record_id!")
+df_with_id.write.mode("overwrite").format("hive").saveAsTable(f"{HIVE_DB}.{TARGET_TABLE}")
 
 # Stop Spark session
 spark.stop()
-
